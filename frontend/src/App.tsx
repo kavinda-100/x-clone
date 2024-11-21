1539df65b797859efef788480da6d591b8633fd5
kavinda-100 <kavindarathnayake100@gmail.com>
2024-11-21 11:26:07 +0530

    init the frontend.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import {Button} from "./components/ui/button";
+
+
+const App = () => {
+    return (
+        <div>
+           <h1 className={"text-violet-600 text-xl font-bold"}> X-Clone</h1>
+            <Button>Click me</Button>
+        </div>
+    );
+};
+
+export default App;
